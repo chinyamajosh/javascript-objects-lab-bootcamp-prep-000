92afe01013b480cfb97c44dd9d3a4b4f0a0106b0
Joshua Chinyama <chinyamajosh@gmail.com>
2018-10-28 17:27:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var recipes = {
+  base: 'Dough',
+  topping: 'Pineapple',
+  cheese: 'Mozzarella'
+};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+   object = {prop: 1, prop2: 2};
+   return object;
+}
+
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+   object[key] = value;
+   return object;
+ }
+  
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var obj ={ prop1: 'prop2'}
+  var newObj= Object.assign({}, obj)
+}
+
+  
+ 
